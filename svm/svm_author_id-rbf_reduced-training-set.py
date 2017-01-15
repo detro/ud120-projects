57348e02cf73db3732d3147042e18370550778ce
Ivan De Marino <idemarino@newrelic.com>
2017-01-15 23:14:21 +0000

    SVM mini project done

--- svm/svm_author_id-rbf_reduced-training-set.py
+++ svm/svm_author_id-rbf_reduced-training-set.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
 
 """
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a Naive Bayes Classifier to identify emails by their authors
-
-    authors and labels:
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
@@ -14,8 +12,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-from sklearn.naive_bayes import GaussianNB
-
+from sklearn.svm import SVC
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -25,8 +22,12 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
-### create classifier
-clf = GaussianNB()
+# Create SVM C-Support Vector Classification classifier
+clf = SVC(kernel="rbf")
+
+# OPTIONAL training set reduction (1% of the original set)
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
 
 ### fit the classifier on the training features and labels
 fitTimeStart = time()
@@ -40,8 +41,10 @@ scoreTimeEnd = time()
 
 print 'Feature TRAINING set size: {0}'.format(len(features_train))
 print 'Feature TEST set size: {0}'.format(len(features_test))
-print 'Accuracy of Gaussian Naive Bayes classifier: {0}'.format(accuracy)
-print 'TRAINING time (GaussianNB.fit()): {0}s'.format(round(fitTimeEnd - fitTimeStart, 3))
-print 'PREDICTION time (GaussianNB.score()): {0}s'.format(round(scoreTimeEnd - scoreTimeStart, 3))
+print 'Accuracy of C-SVC classifier: {0}'.format(accuracy)
+print 'TRAINING time (SVC.fit()): {0}s'.format(round(fitTimeEnd - fitTimeStart, 3))
+print 'PREDICTION time (SVC.score()): {0}s'.format(round(scoreTimeEnd - scoreTimeStart, 3))
 
 #########################################################
+
+

69a1dd639ceb6c4fae854ba4a193280796dd576f
Ivan De Marino <detronizator@gmail.com>
2017-02-26 23:05:21 +0000

    Altered the 'choose your own algorithm' exercise to test all the options
    I have considered both the 3 ones suggested by teachers (KNN, AdaBoost,
    Random Forest) as well as Extremely Randomized Trees, and the ones we
    studied so far.
    
    I have also collected all the results in the RESULTS.md

--- RESULTS.md
+++ RESULTS.md
@@ -2,19 +2,36 @@
 
 ## Chris vs Sara emails ("Enron emails" training set)
 
-| Lesson  | Classifier Algorithm | Accuracy | Training set | Feature set | Training time | Prediction time |
+| Lesson  | Classifier Algorithm | Accuracy | Training set <sup id="a5">[5](#f5)</sup> | Feature set <sup id="a3">[3](#f3)</sup> | Training time | Prediction time |
 | :-----: | :------------------: | :------: | :----------: | :---------: | :-----------: | :-------------: |
-| Naive Bayes | Gaussian Naive Bayes | 0.97326 | 100% <sup id="a5">[5](#f5)</sup> | 10%% <sup id="a3">[3](#f3)</sup> | 2.58s | 0.321s    |
+| Naive Bayes | Gaussian Naive Bayes | 0.97326 | 100% | 10%% | 2.58s | 0.321s    |
 | SVM | SVM (Kernel <sup id="a1">[1](#f1)</sup>: Linear) | 0.98407 | 100% | 10%% | 232.457s | 24.674s |
 | SVM | SVM (Kernel: Linear) | 0.88452 | 10% | 10%% | 0.239s | 1.966s |
 | SVM | SVM (Kernel: Radial Basis Function (RBF)) | 0.61604 | 10% | 10%% | 0.162s | 1.927s |
 | SVM | SVM (Kernel: RBF, C=10 <sup id="a2">[2](#f2)</sup>) | 0.61604 | 10% | 10%% | 0.148s | 1.642s |
 | SVM | SVM (Kernel: RBF, C=100) | 0.61604 | 10% | 10%% | 0.163s | 1.582s |
-| SVM | SVM (Kernel: RBF, C=1000) | 0.82138 | 10% | 10%% | 0.147s | 1.482s |
-| SVM | SVM (Kernel: RBF, C=10000) | 0.89249 | 10% | 10%% | 0.146s | 1.317s |
-| SVM | SVM (Kernel: RBF, C=10000) | 0.99089 | 100% | 10%% | 184.227s | 18.85s |
+| SVM | SVM (Kernel: RBF, C=1K) | 0.82138 | 10% | 10%% | 0.147s | 1.482s |
+| SVM | SVM (Kernel: RBF, C=10K) | 0.89249 | 10% | 10%% | 0.146s | 1.317s |
+| SVM | SVM (Kernel: RBF, C=10K) | 0.99089 | 100% | 10%% | 184.227s | 18.85s |
 | Decision Trees | Decision Tree (Min. Sample Split: 40) | 0.97554 | 100% | 10%% | 123.371s | 0.077s |
 | Decision Trees | Decision Tree (Min. Sample Split: 40) | 0.96700 | 100% | 1%% <sup id="a4">[4](#f4)</sup> | 7.522s | 0.004s |
+
+## Terrain classification
+
+| Lesson  | Classifier Algorithm | Accuracy | Training set <sup id="a6">[6](#f6)</sup> | Feature set <sup id="a7">[7](#f7)</sup> | Training time | Prediction time |
+| :-----: | :------------------: | :------: | :----------: | :---------: | :-----------: | :-------------: |
+| Naive Bayes | Gaussian Naive Bayes | 0.884 | 100% | 2 | **0.001s** | **0.001s** |
+| SVM | SVM (RBF kernel, C=10k) | 0.932 | 100% | 2 | 0.034s | 0.003s |
+| Decision Trees | Decision Tree (Min. Sample Split: 40) | 0.912 | 100% | 2 | 0.002s | **0.001s** |
+| Choose your own Algorithm | K-Nearest Neighbors (default) | 0.92 | 100% | 2 | **0.001s** | 0.002s |
+| Choose your own Algorithm | AdaBoost (default) | 0.924 | 100% | 2 | 0.158s | 0.006s |
+| Choose your own Algorithm | AdaBoost (200 estimators) | 0.916 | 100% | 2 | 0.584s | 0.021s |
+| Choose your own Algorithm | Random Forest (default) | 0.912 | 100% | 2 | 0.034s | 0.009s |
+| Choose your own Algorithm | Random Forest (Min. Sample Split: 40) | 0.924 | 100% | 2 | 0.04s | 0.014s |
+| Choose your own Algorithm | Random Forest (10 estimators, no max depth, Min. Sample Split: 2) | 0.932 | 100% | 2 | 0.044s | 0.012s |
+| Choose your own Algorithm | Extremely Randomized Trees (default) | 0.936 | 100% | 2 | 0.036s | 0.05s |
+| Choose your own Algorithm | Extremely Randomized Trees (Min. Sample Split: 30) | 0.924 | 100% | 2 | 0.041s | 0.009s |
+| Choose your own Algorithm | Extremely Randomized Trees (10 estimators, no max depth, Min. Sample Split: 2) | **0.944** | 100% | 2 | 0.034s | 0.012s |
 
 ## Footnotes
 
@@ -25,4 +42,6 @@
   misclassifying each training example: it's a penalty parameter. [↩](#a2)
 * <sup id="f3">3</sup> `10` percentile of the Features set is `3785`. [↩](#a3)
 * <sup id="f4">4</sup> `1` percentile of the Features set is `379`. [↩](#a4)
-* <sup id="f5">5</sup> Training set contains `15820` data points. [↩](#a4)
+* <sup id="f5">5</sup> Training set contains `15820` data points. [↩](#a5)
+* <sup id="f6">6</sup> Training set contains `750` data points, and Test set contains `250` data points: all auto-generated. [↩](#a6)
+* <sup id="f7">7</sup> Every data point has 2 features: road _grade_ and road _bumpy(ness)_. [↩](#a7)
--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python
 
+from time import time
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+import argparse
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--plot', dest='plot', required=False, action='store_true')
+parser.add_argument('--no-plot', dest='plot', required=False, action='store_false')
+parser.set_defaults(plot=False)
+args = parser.parse_args()
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
-
 
 ### the training data (features_train, labels_train) have both "fast" and "slow"
 ### points mixed together--separate them so we can give them different colors
@@ -15,8 +30,13 @@ bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 
-
 #### initial visualization
+print '*** INPUT DATA:'
+print '* TRAINING set size: {0}'.format(len(features_train))
+print '* TEST set size: {0}'.format(len(features_test))
+print '* FEATURES size: {0}'.format(len(features_train[0]))
+print '***'
+
 plt.xlim(0.0, 1.0)
 plt.ylim(0.0, 1.0)
 plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
@@ -24,21 +44,53 @@ plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
 plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
-plt.show()
+if args.plot: plt.show()
 ################################################################################
 
+# Classifier Algorithms choices, as suggest by teachers
+#
+# 1. k nearest neighbors
+# 2. random forest
+# 3. adaboost (sometimes also called boosted decision tree)
 
-### your code here!  name your classifier object clf if you want the 
+### your code here!  name your classifier object clf if you want the
 ### visualization code (prettyPicture) to show you the decision boundary
 
+classifiers = {
+  'Gaussian Naive Bayes': GaussianNB(),
+  'SVM (RBF kernel, C=10k)': SVC(kernel="rbf", C=10000.0),
+  'Decision Tree (min sample split 40)': DecisionTreeClassifier(min_samples_split=40),
+  'K-Nearest Neighbors (default)': KNeighborsClassifier(),
+  'AdaBoost (default)': AdaBoostClassifier(),
+  'AdaBoost (200 estimators)': AdaBoostClassifier(n_estimators=200),
+  'Random Forest (default)': RandomForestClassifier(),
+  'Random Forest (min sample split 40)': RandomForestClassifier(min_samples_split=40),
+  'Random Forest (10 estimators, no max depth, min sample split 2)': RandomForestClassifier(n_estimators=10, max_depth=None, min_samples_split=2),
+  'Extremely Randomized Trees (default)': ExtraTreesClassifier(),
+  'Extremely Randomized Trees (min sample split 30)': ExtraTreesClassifier(min_samples_split=30),
+  'Extremely Randomized Trees (10 estimators, no max depth, min sample split 2)': ExtraTreesClassifier(n_estimators=10, max_depth=None, min_samples_split=2)
+}
 
+for clfName, clf in classifiers.iteritems():
+  print '*** {0} Classifier results:'.format(clfName)
 
+  ### fit the classifier on the training features and labels
+  fitTimeStart = time()
+  clf.fit(features_train, labels_train)
+  fitTimeEnd = time()
 
+  ### calculate and return the accuracy on the test data
+  scoreTimeStart = time()
+  accuracy = clf.score(features_test, labels_test)
+  scoreTimeEnd = time()
 
+  print '  * Accuracy of {0}: {1}'.format(clfName, accuracy)
+  print '  * TRAINING time ({0}.fit()): {1}s'.format(clfName, round(fitTimeEnd - fitTimeStart, 3))
+  print '  * PREDICTION time ({0}.score()): {1}s'.format(clfName, round(scoreTimeEnd - scoreTimeStart, 3))
+  print '***'
 
-
-
-try:
+  try:
     prettyPicture(clf, features_test, labels_test)
-except NameError:
+    if args.plot: plt.show()
+  except NameError:
     pass
